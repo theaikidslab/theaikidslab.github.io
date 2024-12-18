43ea9bf1cd58ba46764b6059afcd0589bde40f66
Vahid Nikou <Vahid.nikougoftar@gmail.com>
2024-12-18 12:16:42 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './image.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welocme to <b>The AI Kids Lab</b>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Embrace the technology, enlighten the future!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
